24e9cf69242aa007428518cb41cea0fc28c6457f
SuhanaNadeem <suhana.nadeemv@gmail.com>
2020-08-25 13:32:48 -0400

    made sign in button and log in scaffold

--- flashed/lib/main.dart
+++ flashed/lib/main.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+void main()=> runApp(MyApp());
 
-void main() {
-  runApp(
-      MaterialApp(
-        home: Center(child: Text('Flashed'),
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.purple,
       ),
-    ),
-  );
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Google Authentication"),
+        ),
+        body: Center(
+          child: OutlineButton(
+            onPressed: (){},
+            child: Text("Sign in with Google"),
+          ),
+        ),
+      ),
+    );
+  }
 }
