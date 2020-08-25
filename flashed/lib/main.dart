e2b0da811db185f51ead47d26c83a03445b9f184
SuhanaNadeem <suhana.nadeemv@gmail.com>
2020-08-25 12:20:15 -0400

    finally got emulator to work w vscode

--- flashed/lib/main.dart
+++ flashed/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
       MaterialApp(
-        home: Center(child: Text('Hello World'),
+        home: Center(child: Text('Flashed'),
       ),
     ),
   );
