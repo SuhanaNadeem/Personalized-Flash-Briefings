3704e5cd0d11c86322bd981aea33cd26ab12abca
SuhanaNadeem <suhana.nadeemv@gmail.com>
2020-09-04 16:26:33 -0400

    finished register w email and password

--- flashed/lib/screens/authenticate/register.dart
+++ flashed/lib/screens/authenticate/register.dart
@@ -12,10 +12,12 @@ class Register  extends StatefulWidget {
 class _RegisterState extends State<Register > {
 
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // text field state
   String email = "";
   String password = "";
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
@@ -39,10 +41,13 @@ class _RegisterState extends State<Register > {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
         child: Form(
+          key: _formKey,
           child: Column(
             children: <Widget>[
               SizedBox(height:20.0),
               TextFormField(
+                validator: (val) => val.isEmpty ? 'Enter an email' : null,
+
                 onChanged: (val){
                   setState(() => email = val);
                 }
@@ -50,6 +55,7 @@ class _RegisterState extends State<Register > {
               SizedBox(height:20.0),
               TextFormField(
                 obscureText: true,
+                validator: (val) => val.length < 6 ? 'Enter a password 6+ characters long' : null,
                 onChanged: (val){
                   setState(() => password = val);
 
@@ -63,12 +69,22 @@ class _RegisterState extends State<Register > {
                   style: TextStyle(color: Colors.white),
                 ),
                 onPressed: () async{
-                  print(email);
-                  print(password);
+                  if (_formKey.currentState.validate()){
+                    dynamic result = await _auth.registerWithEmailAndPassword(email, password);
+                    
+                    if(result == null){
+                      setState(() => error = 'Please supply a valid email');
+                    }
+                  }
                 }
-              )
-            ]
-          )
+              ),
+              SizedBox(height: 12.0),
+              Text(
+                error,
+                style: TextStyle(color:Colors.red, fontSize: 14.0),
+              ),
+            ],
+          ),
         ),
       ),
     );
--- flashed/lib/services/auth.dart
+++ flashed/lib/services/auth.dart
@@ -31,6 +31,18 @@ class AuthService {
 
   //sign in w email and pw
   //register w email & pw
+
+  Future registerWithEmailAndPassword(String email, String password) async {
+    try {
+      AuthResult result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+      return _userFromFirebaseUser(user);
+    } catch(e){
+      print(e.toString());
+      return null;
+    }
+  }
+
   //sign out
 
   Future signOut() async {
